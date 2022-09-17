f63136a79d94490a54a148cdb9971dea5239f986
rafsun <rafsundipto116@gmail.com>
2022-09-17 14:53:39 +0600

    front-end file upload

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const UpdateUser = () => {
+    const [user, setUser] = useState({});
+    const { id } = useParams();
+    useEffect(() => {
+        const url = `http://localhost:5000/users/${id}`;
+        fetch(url)
+            .then(res=> res.json())
+            .then(data=> setUser(data));
+    }, [])
+    //update user
+    const handelNameChange = e => {
+        const updatedName = e.target.value;
+        const updatedUser = { name: updatedName, email: user.email };
+        setUser(updatedUser);
+    }
+    const handelEmailChange = e => {
+        const updatedEmail = e.target.value;
+        // const updatedUser = { ...user };
+        // updatedUser.email = updatedEmail;
+        const updatedUser = { name: user.name, email:updatedEmail }
+        setUser(updatedUser)
+    }
+    const handelUpdateUser = e => {
+       const uri = `http://localhost:5000/users/${id}`
+        fetch(uri, {
+            method:'PUT',
+            headers: {
+               'Content-Type':'application/json'
+            },
+            body:JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    alert('updated successfully')
+                    setUser({});
+            }
+        })
+        e.preventDefault();
+    }
+
     return (
         <div>
-            <h2>This is Update User</h2>
+            <h2>update: {user.name}  {user.email}</h2>
+            <p><small>{id}</small></p>
+            <form onSubmit={handelUpdateUser}>
+                <input type="name" onChange={handelNameChange} value={user.name || ''}/>
+                <input type="email" onChange={handelEmailChange} value={user.email || ''} />
+                <input type="submit"  value='update'/>
+
+
+            </form>
         </div>
     );
 };
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, []);
+    //Delete users
+    const handelDeleteUser = id => {
+        const proceed = window.confirm('Are you sure, you want to delete?');
+        if (proceed) {
+             const url = `http://localhost:5000/users/${id}`;
+        fetch(url, {
+            method:'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    alert('delete successfully');
+                    const remainingUsers = users.filter(user => user._id !== id)
+                    setUsers(remainingUsers);
+            }
+        })
+        }
+
+}
+
     return (
+
         <div>
-            <h2>This is Users</h2>
+            <h2>available users:{users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li key={user._id} >{user.name} :: {user.email} : :
+                        <Link to={`/users/update/${user._id}`}><button>update</button></Link>
+                        <button onClick={()=> handelDeleteUser(user._id)}>X</button>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
